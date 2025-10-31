143526ad5778747fd4ed7c9f310106d7aba35fe0
Andy Inman <andy@lastcallmedia.com>
2025-10-31 20:37:59 +0000

    Added simple shell script to generate CSV data for a range of tests.

---
+++ csv.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+php tests/bit_storage_test.php 0  10000000 1  1
+php tests/bit_storage_test.php 0  20000000 1  1
+php tests/bit_storage_test.php 0  30000000 1  1
+php tests/bit_storage_test.php 0  40000000 1  1
+php tests/bit_storage_test.php 0  50000000 1  1
+php tests/bit_storage_test.php 0  60000000 1  1
+php tests/bit_storage_test.php 0  70000000 1  1
+php tests/bit_storage_test.php 0  80000000 1  1
+php tests/bit_storage_test.php 0  90000000 1  1
+php tests/bit_storage_test.php 0 100000000 1  1
